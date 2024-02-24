350342fd490725a47e965d1592324a7df5bd6767
Dawid Zimoch <dawid.jerzy.zimoch@gmail.com>
2024-02-24 21:13:05 +0100

    Initial Commit

---
+++ SnakeGame-SwiftUI-MVVM/SnakeGame_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGame_SwiftUI_MVVMApp.swift
+//  SnakeGame-SwiftUI-MVVM
+//
+//  Created by Dawid Zimoch on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGame_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
